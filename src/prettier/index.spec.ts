480ebdee11547fcf87167c53c09c29214a7eb264
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-03-06 13:07:06 +0100

    pretier: add package to devDependencies

--- src/prettier/index.spec.ts
+++ src/prettier/index.spec.ts
@@ -1,14 +1,31 @@
-import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import { Tree, VirtualTree } from '@angular-devkit/schematics';
+import {
+  SchematicTestRunner,
+  UnitTestTree
+} from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
 const collectionPath = path.join(__dirname, '../collection.json');
 
-describe('schematics', () => {
-  it('works', () => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = runner.runSchematic('schematics', {}, Tree.empty());
+describe('@co-it/schematics:prettier', () => {
+  describe('When prettier is not installed', () => {
+    let runner: SchematicTestRunner;
+    let actualTree: Tree;
 
-    expect(tree.files).toEqual([]);
+    beforeEach(() => {
+      runner = new SchematicTestRunner('prettier', collectionPath);
+      actualTree = new UnitTestTree(new VirtualTree());
+    });
+
+    it('should add prettier to the devDependencies', () => {
+      const packageBeforeInstall = { devDependencies: {} };
+      actualTree.create('package.json', JSON.stringify(packageBeforeInstall));
+
+      const tree = runner.runSchematic('prettier', {}, actualTree);
+
+      const packageAfterInstall = JSON.parse(tree.readContent('package.json'));
+
+      expect(packageAfterInstall.devDependencies).toHaveProperty('prettier');
+    });
   });
 });
--- src/prettier/index.ts
+++ src/prettier/index.ts
@@ -2,6 +2,17 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 export default function(): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    const buffer = tree.read('package.json');
+
+    if (!buffer) {
+      return tree;
+    }
+
+    const nodePackageDefinitions = JSON.parse(buffer.toString('utf-8'));
+    nodePackageDefinitions.devDependencies.prettier = '@latest';
+
+    tree.overwrite('package.json', JSON.stringify(nodePackageDefinitions));
+
     return tree;
   };
 }
