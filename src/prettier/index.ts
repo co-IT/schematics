37f030bd8ecce66cfd6a3791fd77291f92cf00ce
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-03-06 13:58:52 +0100

    ref(prettier): extract package actions into own class

--- src/prettier/index.ts
+++ src/prettier/index.ts
@@ -9,16 +9,40 @@ export default function(): Rule {
       return tree;
     }
 
-    const nodePackageDefinitions = JSON.parse(buffer.toString('utf-8'));
-    nodePackageDefinitions.devDependencies.prettier = '@latest';
+    const packageJson = new PackageJson(buffer);
+    packageJson.setDevDependency('prettier', '@latest');
+    packageJson.setScript(
+      'format',
+      'prettier --write "**/*.{js,json,css,scss,md,ts,html}\''
+    );
 
-    nodePackageDefinitions.scripts.format =
-      'prettier --write "**/*.{js,json,css,scss,md,ts,html}\'';
-
-    tree.overwrite('package.json', JSON.stringify(nodePackageDefinitions));
+    tree.overwrite('package.json', packageJson.stringify());
 
     _context.addTask(new NodePackageInstallTask());
 
     return tree;
   };
+}
+
+class PackageJson {
+  private _config: {
+    devDependencies: { [key: string]: string };
+    scripts: { [key: string]: string };
+  };
+
+  constructor(buffer: Buffer) {
+    this._config = JSON.parse(buffer.toString('utf-8'));
+  }
+
+  setDevDependency(name: string, version: string): void {
+    this._config.devDependencies[name] = version;
+  }
+
+  setScript(name: string, command: string): void {
+    this._config.scripts[name] = command;
+  }
+
+  stringify(): string {
+    return JSON.stringify(this._config);
+  }
 }
