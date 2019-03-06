297e5f901481a1bdc0e6f39df937fc2694064c6d
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-03-06 12:10:12 +0100

    chore: use jest instead of jasmine

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '\\.spec\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+};
