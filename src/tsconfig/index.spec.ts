afef57933e62711cfaea6ded4d1d5662564c4488
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-03-07 17:29:33 +0100

    fix(tsconfig): correct name of tsconfig file

--- src/tsconfig/index.spec.ts
+++ src/tsconfig/index.spec.ts
@@ -1,11 +1,14 @@
-import { SchematicsException } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import { EmptyTree, SchematicsException } from '@angular-devkit/schematics';
+import {
+  SchematicTestRunner,
+  UnitTestTree
+} from '@angular-devkit/schematics/testing';
 import { join } from 'path';
 
 const collectionPath = join(__dirname, '../collection.json');
 
 describe('@co-it/schematics:tsconfig', () => {
-  describe('When no root tsconfig.json is present', () => {
+  describe('When tsconfig.json is not present', () => {
     it('should fail', () => {
       const runner = new SchematicTestRunner('schematics', collectionPath);
 
@@ -14,6 +17,18 @@ describe('@co-it/schematics:tsconfig', () => {
           'Sorry, no tsconfig.json was found in root directory.'
         )
       );
+    });
+  });
+
+  describe('When tsconfig.json is present', () => {
+    it('should should set "strict: true"', () => {
+      const project = new UnitTestTree(new EmptyTree());
+      project.create('tsconfig.json', JSON.stringify({}));
+      const runner = new SchematicTestRunner('schematics', collectionPath);
+      const tree = runner.runSchematic('tsconfig', {}, project);
+      const tsconfig = JSON.parse(tree.readContent('tsconfig.json'));
+
+      expect(tsconfig.compilerOptions.strict).toBe(true);
     });
   });
 });
