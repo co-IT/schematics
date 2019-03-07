5c8d564a85b5943c0091f70fcd4763b9e6156722
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-03-07 17:41:34 +0100

    tsconfig: set strict mode to true

--- src/tsconfig/index.ts
+++ src/tsconfig/index.ts
@@ -4,14 +4,23 @@ import {
   SchematicsException,
   Tree
 } from '@angular-devkit/schematics';
+import { TsConfigSchema } from './tsconfig.schema';
 
-export default function(): Rule {
+export default function(parameters: TsConfigSchema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    if (!tree.exists('.tsconfig')) {
+    if (!tree.exists('tsconfig.json')) {
       throw new SchematicsException(
         'Sorry, no tsconfig.json was found in root directory.'
       );
     }
+
+    const tsconfig = JSON.parse(tree.read('tsconfig.json')!.toString('utf-8'));
+
+    tsconfig.compilerOptions = tsconfig.compilerOptions || {};
+    tsconfig.compilerOptions.strict = parameters.strict;
+
+    tree.overwrite('tsconfig.json', JSON.stringify(tsconfig));
+
     return tree;
   };
 }
