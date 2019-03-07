f43fc16e2e6c0db90cbb6e1567c805dfa48f5177
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-03-07 17:52:35 +0100

    tsconfig: set noUnusedParameters to true by default

--- src/tsconfig/index.spec.ts
+++ src/tsconfig/index.spec.ts
@@ -4,10 +4,18 @@ import {
   UnitTestTree
 } from '@angular-devkit/schematics/testing';
 import { join } from 'path';
+import { TsConfigSchema } from './tsconfig.schema.js';
+import * as schema from './tsconfig.schema.json';
 
 const collectionPath = join(__dirname, '../collection.json');
 
 describe('@co-it/schematics:tsconfig', () => {
+  const parameterDefaults: TsConfigSchema = {
+    strict: schema.properties.strict.default,
+    noUnusedLocals: schema.properties.noUnusedLocals.default,
+    noUnusedParameters: schema.properties.noUnusedParameters.default
+  };
+
   describe('When tsconfig.json is not present', () => {
     it('should fail', () => {
       const runner = new SchematicTestRunner('schematics', collectionPath);
@@ -25,10 +33,20 @@ describe('@co-it/schematics:tsconfig', () => {
       const project = new UnitTestTree(new EmptyTree());
       project.create('tsconfig.json', JSON.stringify({}));
       const runner = new SchematicTestRunner('schematics', collectionPath);
-      const tree = runner.runSchematic('tsconfig', { strict: true }, project);
+      const tree = runner.runSchematic('tsconfig', parameterDefaults, project);
       const tsconfig = JSON.parse(tree.readContent('tsconfig.json'));
 
       expect(tsconfig.compilerOptions.strict).toBe(true);
+    });
+
+    it('should should set "noUnusedParameters: true"', () => {
+      const project = new UnitTestTree(new EmptyTree());
+      project.create('tsconfig.json', JSON.stringify({}));
+      const runner = new SchematicTestRunner('schematics', collectionPath);
+      const tree = runner.runSchematic('tsconfig', parameterDefaults, project);
+      const tsconfig = JSON.parse(tree.readContent('tsconfig.json'));
+
+      expect(tsconfig.compilerOptions.noUnusedParameters).toBe(true);
     });
   });
 });
