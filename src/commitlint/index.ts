2d468b96d9f0b338da6e202e52d31f472e7659c0
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2019-03-08 12:22:01 +0100

    commitlint: add husky hook rule

--- src/commitlint/index.ts
+++ src/commitlint/index.ts
@@ -20,12 +20,25 @@ export default function commitlint(_options: any): Rule {
         'husky'
       ]
     }),
-    addCommitlintConfig()
+    addCommitlintConfig(),
+    addHuskyHook()
   ]);
 }
 
 function addCommitlintConfig(): Rule {
   return () => {
     return chain([mergeWith(apply(url('./templates'), [move('')]))]);
+  };
+}
+
+function addHuskyHook(): Rule {
+  return (tree: Tree) => {
+    const packageJson = new PackageJson(tree.read('package.json'));
+
+    packageJson.setHuskyHook('commit-msg', 'commitlint -E HUSKY_GIT_PARAMS');
+
+    tree.overwrite('package.json', packageJson.stringify());
+
+    return tree;
   };
 }
