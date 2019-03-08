0ef16934118a02f504367d510436b93f6a927870
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2019-03-08 11:31:28 +0100

    ref: extract shared rule for installing dependencies

---
+++ src/lib/rules/install-dependencies.spec.ts
@@ -0,0 +1,48 @@
+import { SchematicContext, VirtualTree } from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import { UnitTestTree } from '@angular-devkit/schematics/testing';
+import { installDependencies } from './install-dependencies';
+
+describe('installDependencies', () => {
+  describe('When dependency is not installed', () => {
+    let tree: UnitTestTree;
+    let mockContext: Partial<SchematicContext>;
+
+    beforeEach(() => {
+      tree = new UnitTestTree(new VirtualTree());
+
+      const packageBeforeInstall = { scripts: {}, devDependencies: {} };
+      tree.create('package.json', JSON.stringify(packageBeforeInstall));
+
+      mockContext = {
+        addTask: jest.fn()
+      };
+    });
+
+    it('should add devDependencies to package.json', () => {
+      const rule = installDependencies({
+        devDependencies: ['test-dependency1', 'test-dependency2']
+      });
+      rule(tree, mockContext as SchematicContext);
+
+      const packageAfterInstall = JSON.parse(tree.readContent('package.json'));
+      expect(packageAfterInstall.devDependencies).toEqual(
+        expect.objectContaining({
+          'test-dependency1': 'latest',
+          'test-dependency2': 'latest'
+        })
+      );
+    });
+
+    it('should add a NodePackageInstallTask to context', () => {
+      const rule = installDependencies({
+        devDependencies: ['test-dependency']
+      });
+      rule(tree, mockContext as SchematicContext);
+
+      expect(mockContext.addTask).toHaveBeenCalledWith(
+        expect.any(NodePackageInstallTask)
+      );
+    });
+  });
+});
