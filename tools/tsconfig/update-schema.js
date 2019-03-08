c216a01de2bea7269c81722a84265f9ea7d6c873
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-03-08 15:44:38 +0100

    tsconfig: add script downloading schema for tsconfig.json

---
+++ tools/tsconfig/update-schema.js
@@ -0,0 +1,20 @@
+const write = require('fs-extra').writeFile;
+const fetch = require('node-fetch');
+
+/**
+ * Downloads the schema for tsconfig.json
+ * @param url download source for schema file
+ */
+function downloadSchema(url) {
+  return fetch(url)
+    .then(response => response.json())
+    .then(schema => {
+      delete schema.$schema;
+      return schema;
+    })
+    .then(schema => JSON.stringify(schema, null, 2));
+}
+
+downloadSchema('http://json.schemastore.org/tsconfig')
+  .then(schema => write('./tools/tsconfig/schema.json', schema))
+  .catch(err => console.log(err));
