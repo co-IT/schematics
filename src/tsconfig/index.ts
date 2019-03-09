e9b0411250d1894fe53d992d45645fa78c53955e
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-03-09 16:01:50 +0100

    ref(tsconfig): remove unsused parameter

--- src/tsconfig/index.ts
+++ src/tsconfig/index.ts
@@ -1,13 +1,8 @@
-import {
-  Rule,
-  SchematicContext,
-  SchematicsException,
-  Tree
-} from '@angular-devkit/schematics';
+import { Rule, SchematicsException, Tree } from '@angular-devkit/schematics';
 import { CompilerOptions } from './models';
 
 export default function(parameters: CompilerOptions): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
+  return (tree: Tree) => {
     if (!tree.exists('tsconfig.json')) {
       throw new SchematicsException(
         'Sorry, no tsconfig.json was found in root directory.'
