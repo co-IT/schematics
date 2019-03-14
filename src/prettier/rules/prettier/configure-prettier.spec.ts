f59b9ae204cdee3ee0488f5d09593fc7f44f591b
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-03-14 19:00:52 +0100

    docs(prettier): add apply to the list of things that cannot be tested yet

--- src/prettier/rules/prettier/configure-prettier.spec.ts
+++ src/prettier/rules/prettier/configure-prettier.spec.ts
@@ -3,7 +3,11 @@
  * Intentionally left blank
  *
  * Until now, we do not know how to test a rule that uses templates.
- * We need to fake url() ["@angular-devkit/schematics" ]
+ * We need to fake url() & apply() ["@angular-devkit/schematics" ]
  * to return a pre configured Source.
  *
  */
+
+describe.skip('configure-prettier', () => {
+  it('should be tested as unit', () => {});
+});
