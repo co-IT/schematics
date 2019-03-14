3f4b2e365bb6f4b9681f7ed43f61f45c2a331a1e
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-03-14 14:18:54 +0100

    style(prettier): add missing newline

---
+++ src/prettier/rules/prettier/configure-prettier.spec.ts
@@ -0,0 +1,9 @@
+/**
+ *
+ * Intentionally left blank
+ *
+ * Until now, we do not know how to test a rule that uses templates.
+ * We need to fake url() ["@angular-devkit/schematics" ]
+ * to return a pre configured Source.
+ *
+ */
