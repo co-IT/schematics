e0f74ee8702b510a5741bd46c2cef6a02e60611b
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-03-14 20:45:15 +0100

    test(prettier/tslint): enforce single quotes

--- src/prettier/rules/tslint/patch-ts-lint-configuration.spec.ts
+++ src/prettier/rules/tslint/patch-ts-lint-configuration.spec.ts
@@ -1,18 +1,31 @@
-import { SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { UnitTestTree } from '@angular-devkit/schematics/testing';
 import { patchTsLintConfiguration } from './patch-ts-lint-configuration';
 
 describe('When "extends" does not exist', () => {
-  it('should add the property and add prettier tslint configuration', () => {
-    const rule = patchTsLintConfiguration();
-    const emptyContext: SchematicContext = {} as SchematicContext;
-    const tree = new UnitTestTree(Tree.empty());
-    tree.create('tslint.json', JSON.stringify({}));
+  let rule: Rule;
+  let emptyContext: SchematicContext;
+  let tree: UnitTestTree;
 
+  beforeEach(() => {
+    rule = patchTsLintConfiguration();
+    emptyContext = {} as SchematicContext;
+    tree = new UnitTestTree(Tree.empty());
+    tree.create('tslint.json', JSON.stringify({}));
+  });
+
+  it('should add the property and add prettier tslint configuration', () => {
     rule(tree, emptyContext);
 
     const tslintJson = JSON.parse(tree.readContent('tslint.json'));
     expect(tslintJson.extends).toContain('tslint-config-prettier');
+  });
+
+  it('should enforce single quotes', () => {
+    rule(tree, emptyContext);
+
+    const tslintJson = JSON.parse(tree.readContent('tslint.json'));
+    expect(tslintJson.rules.quotemark).toEqual([true, 'single']);
   });
 });
 
--- src/prettier/rules/tslint/patch-ts-lint-configuration.ts
+++ src/prettier/rules/tslint/patch-ts-lint-configuration.ts
@@ -1,19 +1,34 @@
 import { Rule, Tree } from '@angular-devkit/schematics';
 
+const opinionatedTsLintRules = {
+  rules: {
+    quotemark: [true, 'single'],
+    'arrow-parens': false,
+    'max-line-length': [true, 80],
+    'trailing-comma': [true, { multiline: 'never', singleline: 'never' }]
+  }
+};
+
 export function patchTsLintConfiguration(): Rule {
   return (tree: Tree) => {
-    const tslintFile = tree.read('tslint.json');
-    if (!tslintFile) {
+    const originTsLintFile = tree.read('tslint.json');
+    if (!originTsLintFile) {
       return;
     }
-    const tslintJson = JSON.parse(tslintFile!.toString('utf-8'));
-    if (Array.isArray(tslintJson.extends)) {
-      tslintJson.extends.push('tslint-config-prettier');
-    } else if (tslintJson.extends) {
-      tslintJson.extends = [tslintJson.extends, 'tslint-config-prettier'];
+    const originTsLintJson = JSON.parse(originTsLintFile.toString('utf-8'));
+    if (Array.isArray(originTsLintJson.extends)) {
+      originTsLintJson.extends.push('tslint-config-prettier');
+    } else if (originTsLintJson.extends) {
+      originTsLintJson.extends = [
+        originTsLintJson.extends,
+        'tslint-config-prettier'
+      ];
     } else {
-      tslintJson.extends = ['tslint-config-prettier'];
+      originTsLintJson.extends = ['tslint-config-prettier'];
     }
+
+    const tslintJson = { ...originTsLintJson, ...opinionatedTsLintRules };
+
     tree.overwrite('tslint.json', JSON.stringify(tslintJson, null, 2));
   };
 }
