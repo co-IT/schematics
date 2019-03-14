abed15af90ea3dc9ec44d4792282bf2e3182934f
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-03-14 20:48:07 +0100

    ref(prettier/tslint): extract tslint rules into own file

--- src/prettier/rules/tslint/patch-ts-lint-configuration.ts
+++ src/prettier/rules/tslint/patch-ts-lint-configuration.ts
@@ -1,21 +1,15 @@
 import { Rule, Tree } from '@angular-devkit/schematics';
-
-const opinionatedTsLintRules = {
-  rules: {
-    quotemark: [true, 'single'],
-    'arrow-parens': false,
-    'max-line-length': [true, 80],
-    'trailing-comma': [true, { multiline: 'never', singleline: 'never' }]
-  }
-};
+import { opinionatedTsLintRules } from './opinionated-tslint-rules';
 
 export function patchTsLintConfiguration(): Rule {
   return (tree: Tree) => {
     const originTsLintFile = tree.read('tslint.json');
+
     if (!originTsLintFile) {
       return;
     }
     const originTsLintJson = JSON.parse(originTsLintFile.toString('utf-8'));
+
     if (Array.isArray(originTsLintJson.extends)) {
       originTsLintJson.extends.push('tslint-config-prettier');
     } else if (originTsLintJson.extends) {
