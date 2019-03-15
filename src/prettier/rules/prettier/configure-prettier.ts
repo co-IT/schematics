9910d7612fc0a2cc37b7988091a86a4b03ec6a49
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-03-15 17:51:10 +0100

    fix(prettier): overwrite existing .prettierrc
    - workaorund introduce due https://github.com/angular/angular-cli/issues/11337

--- src/prettier/rules/prettier/configure-prettier.ts
+++ src/prettier/rules/prettier/configure-prettier.ts
@@ -1,18 +1,31 @@
 import {
   apply,
   chain,
+  forEach,
   MergeStrategy,
   mergeWith,
   Rule,
+  Tree,
   url
 } from '@angular-devkit/schematics';
 import { installDependencies } from '../../../lib';
 
 export function configurePrettier(): Rule {
-  return chain([
-    installDependencies({
-      devDependencies: ['prettier', 'tslint-config-prettier']
-    }),
-    mergeWith(apply(url('./templates/prettier'), []), MergeStrategy.Overwrite)
-  ]);
+  return (tree: Tree) =>
+    chain([
+      installDependencies({
+        devDependencies: ['prettier', 'tslint-config-prettier']
+      }),
+      mergeWith(
+        apply(url('./templates/prettier'), [
+          forEach(template => {
+            tree.exists(template.path)
+              ? tree.overwrite(template.path, template.content)
+              : tree.create(template.path, template.content);
+            return null;
+          })
+        ]),
+        MergeStrategy.Overwrite
+      )
+    ]);
 }
