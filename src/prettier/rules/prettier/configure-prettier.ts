55705e203e74addf570e6eb98913001122b3e2f6
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-03-15 09:30:12 +0100

    ref(prettier): move installation of dependencies to configure-prettier

--- src/prettier/rules/prettier/configure-prettier.ts
+++ src/prettier/rules/prettier/configure-prettier.ts
@@ -1,11 +1,18 @@
 import {
   apply,
+  chain,
   MergeStrategy,
   mergeWith,
   Rule,
   url
 } from '@angular-devkit/schematics';
+import { installDependencies } from '../../../lib';
 
 export function configurePrettier(): Rule {
-  return mergeWith(apply(url('./templates'), []), MergeStrategy.Overwrite);
+  return chain([
+    installDependencies({
+      devDependencies: ['prettier', 'tslint-config-prettier']
+    }),
+    mergeWith(apply(url('./templates'), []), MergeStrategy.Overwrite)
+  ]);
 }
