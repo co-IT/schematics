08be82e8f3000144782cd9ad97ef9a778329c2cc
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-03-15 09:30:12 +0100

    ref(prettier): move installation of dependencies to configure-prettier

--- src/prettier/index.ts
+++ src/prettier/index.ts
@@ -1,5 +1,4 @@
 import { chain, Rule } from '@angular-devkit/schematics';
-import { installDependencies } from '../lib';
 import {
   configurePrettier,
   registerPrettier,
@@ -9,9 +8,6 @@ import { patchTsLintConfiguration } from './rules/tslint';
 
 export default function(): Rule {
   return chain([
-    installDependencies({
-      devDependencies: ['prettier', 'tslint-config-prettier']
-    }),
     configurePrettier(),
     warnAgainstCompetingPrettierConfiguration(),
     registerPrettier(),
