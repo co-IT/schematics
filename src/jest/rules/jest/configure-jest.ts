15b95f3ae3599cdfec82c2413bb0d2f093b5c564
Frank Strieter <fs@co-it.eu>
2019-03-19 08:53:35 +0100

    feat(jest): add templates for configuration files

---
+++ src/jest/rules/jest/configure-jest.ts
@@ -0,0 +1,40 @@
+import {
+  apply,
+  chain,
+  forEach,
+  MergeStrategy,
+  mergeWith,
+  Rule,
+  Tree,
+  url,
+} from '@angular-devkit/schematics';
+import { installDependencies } from '../../../lib';
+
+/**
+ * TODO: Workaround
+ * ----------------
+ * The operator `forEach` should not be needed to overwrite a file.
+ * There is a bug in the devkit: https://github.com/angular/angular-cli/issues/11337
+ * The MergeStrategy.Overwrite takes no effect.
+ *
+ * Once the issue is resolved `forEach` should be removed.
+ */
+export function configureJest(): Rule {
+  return (tree: Tree) =>
+    chain([
+      installDependencies({
+        devDependencies: ['jest', 'jest-preset-angular'],
+      }),
+      mergeWith(
+        apply(url('./templates/jest'), [
+          forEach(template => {
+            tree.exists(template.path)
+              ? tree.overwrite(template.path, template.content)
+              : tree.create(template.path, template.content);
+            return null;
+          }),
+        ]),
+        MergeStrategy.Overwrite
+      ),
+    ]);
+}
