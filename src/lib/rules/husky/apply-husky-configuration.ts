4d1871e9a3bfb0b2faebc9d64af2eda515dff32b
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2019-03-19 07:13:41 +0100

    docs(commitlint): add jsdoc to applyHuskyConfiguration rule

--- src/lib/rules/husky/apply-husky-configuration.ts
+++ src/lib/rules/husky/apply-husky-configuration.ts
@@ -2,6 +2,16 @@ import { Rule, Tree } from '@angular-devkit/schematics';
 import { createHuskyRc } from './create-husky-rc';
 import { updateHuskyRc } from './update-husky-rc';
 
+/**
+ * Create or update husky configuration with the new hook.
+ * Uses the template in ./templates/hook.
+ *
+ * **Note**: newHook is currently needed to be provided for updating existing
+ * huskyrc files due to a bug in MergeStrategy.Overwrite.
+ * See https://github.com/angular/angular-cli/issues/11337 for details.
+ *
+ * @param newHook key value pair representing the hook to add
+ */
 export function applyHuskyConfiguration(newHook: [string, string]): Rule {
   return (tree: Tree) =>
     !tree.exists('.huskyrc')
