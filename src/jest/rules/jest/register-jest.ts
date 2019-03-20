d4564cdd79e7e5cc3549dc8a1c8c457614105e0e
Frank Strieter <fs@co-it.eu>
2019-03-20 16:10:02 +0100

    feat(jest): reorder scripts for package.json

--- src/jest/rules/jest/register-jest.ts
+++ src/jest/rules/jest/register-jest.ts
@@ -4,8 +4,8 @@ import { Rule, Tree } from '@angular-devkit/schematics';
 export function registerJest(): Rule {
   return (tree: Tree) => {
     const packageJson = new PackageJson(tree.read('package.json'));
-    packageJson.setScript('test:watch', 'jest --watch true');
     packageJson.setScript('test', 'jest');
+    packageJson.setScript('test:watch', 'jest --watch');
     tree.overwrite('package.json', packageJson.stringify());
   };
 }
