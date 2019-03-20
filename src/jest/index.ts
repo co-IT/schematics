7296ee0b89c6430fa6a835b2b6a76b781a5ba05a
Frank Strieter <fs@co-it.eu>
2019-03-20 21:13:41 +0100

    feat(jest): configure typescript compiler for jest

--- src/jest/index.ts
+++ src/jest/index.ts
@@ -1,6 +1,7 @@
 import { Rule, chain } from '@angular-devkit/schematics';
 import { configureJest, registerJest } from './rules/jest';
+import { patchTsConfig } from './rules/tsconfig/patch-tsconfig';
 
 export default function(): Rule {
-  return chain([configureJest(), registerJest()]);
+  return chain([configureJest(), registerJest(), patchTsConfig()]);
 }
---
+++ src/jest/rules/tsconfig/patch-tsconfig.ts
@@ -0,0 +1,35 @@
+import { Tree, Rule } from '@angular-devkit/schematics';
+
+type TsConfigPatchOptions = {
+  compilerOptions: {
+    types: string[];
+    module: string;
+  };
+  files: string[];
+};
+
+export function patchTsConfig(): Rule {
+  return (tree: Tree) => {
+    const file = 'src/tsconfig.spec.json';
+    const buffer = tree.read(file);
+    if (!buffer) {
+      throw new Error(`No ${file} found`);
+    }
+    const tsconfig: TsConfigPatchOptions = JSON.parse(buffer.toString('utf-8'));
+    tsconfig.compilerOptions = tsconfig.compilerOptions || {
+      types: [],
+      module: ''
+    };
+    tsconfig.files = tsconfig.files || [];
+
+    tsconfig.compilerOptions.types.splice(
+      tsconfig.compilerOptions.types.indexOf('jasmine'),
+      1,
+      'jest'
+    );
+    tsconfig.compilerOptions.module = 'commonjs';
+    tsconfig.files.splice(tsconfig.files.indexOf('test.ts'), 1);
+
+    tree.overwrite(file, JSON.stringify(tsconfig, null, 2));
+  };
+}
