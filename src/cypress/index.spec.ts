b82e64b8bcf677efb84ae39b26e4c7e9390c1610
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2019-03-23 12:07:03 +0100

    feat(cypress): generate new e2e app if overwrite parameter is false

--- src/cypress/index.spec.ts
+++ src/cypress/index.spec.ts
@@ -390,5 +390,70 @@ describe('@co-it/schematics:cypress', () => {
         );
       });
     });
+
+    describe('When user wants to create new e2e project', () => {
+      it.each`
+        root                 | expected
+        ${''}                | ${'my-app-e2e/'}
+        ${'/'}               | ${'my-app-e2e/'}
+        ${'my-app/'}         | ${'my-app-e2e/'}
+        ${'projects/my-app'} | ${'projects/my-app-e2e/'}
+      `(
+        'should set e2e project root to "%expected" in angular.json for app with root "%root"',
+        ({ root, expected }: { root: string; expected: string }) => {
+          const parameters = {
+            ...defaultParameters,
+            app: 'my-app'
+          };
+
+          treeBefore.create(
+            'angular.json',
+            JSON.stringify({
+              projects: {
+                'my-app': {
+                  root,
+                  projectType: 'application',
+                  architect: {}
+                }
+              }
+            })
+          );
+
+          const tree = runner.runSchematic('cypress', parameters, treeBefore);
+
+          const angularJson = JSON.parse(tree.readContent('angular.json'));
+
+          expect(angularJson.projects['my-app-e2e'].root).toEqual(expected);
+        }
+      );
+
+      it('should throw an exception if new root folder already exists', () => {
+        const parameters = {
+          ...defaultParameters,
+          app: 'my-app'
+        };
+
+        treeBefore.create('my-app-e2e', '');
+        treeBefore.create('my-app-e2e/.keep', '');
+        treeBefore.create(
+          'angular.json',
+          JSON.stringify({
+            projects: {
+              'my-app': {
+                root: '',
+                projectType: 'application',
+                architect: {}
+              }
+            }
+          })
+        );
+
+        expect(() =>
+          runner.runSchematic('cypress', parameters, treeBefore)
+        ).toThrowError(
+          'Could not create root folder "my-app-e2e/" because it is not empty.'
+        );
+      });
+    });
   });
 });
