93578a64f303f25632ee58dfaa40fc1b8086d574
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2019-03-23 12:07:03 +0100

    feat(cypress): generate new e2e app if overwrite parameter is false

--- src/cypress/index.ts
+++ src/cypress/index.ts
@@ -88,18 +88,50 @@ function configureAngularJson(
 ): Rule {
   return (tree: Tree) => {
     const e2eAppName = getE2eProjectNameForApp(options.app);
-    const root = angularJson.getApp(e2eAppName).root;
+
+    const root = options.overwrite
+      ? angularJson.getApp(e2eAppName).root
+      : calculateRootFolderFromTargetApp(options, angularJson);
 
     const newAppConfiguration: AngularJsonProject = createCypressProjectConfiguration(
       root,
       options.app
     );
 
+    if (!options.overwrite) {
+      tree.getDir(newAppConfiguration.root).visit(() => {
+        // tree.exists is false for directories, so I didn't
+        // find another way to check if directory is empty...
+        throw new SchematicsException(
+          `Could not create root folder "${
+            newAppConfiguration.root
+          }" because it is not empty.`
+        );
+      });
+    }
+
     angularJson.setApp(e2eAppName, newAppConfiguration);
 
     tree.overwrite('angular.json', angularJson.stringify());
     return tree;
   };
+}
+
+function calculateRootFolderFromTargetApp(
+  options: CypressSchematicOptions,
+  angularJson: AngularJson
+) {
+  const targetAppRoot = angularJson.getApp(options.app).root;
+  const sanitizedTargetAppRoot = targetAppRoot.endsWith('/')
+    ? targetAppRoot
+    : `${targetAppRoot}/`;
+  if (sanitizedTargetAppRoot === '/') {
+    return `${getE2eProjectNameForApp(options.app)}/`;
+  }
+
+  const pathSegments = sanitizedTargetAppRoot.split('/');
+  pathSegments[pathSegments.length - 2] = getE2eProjectNameForApp(options.app);
+  return pathSegments.join('/');
 }
 
 function createCypressProjectConfiguration(
